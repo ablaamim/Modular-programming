384034833ad777f106b113e71a42ba0e2ca35dc6
alaamimi <aku-no-hana@outlook.fr>
2021-09-15 00:28:44 +0100

    concat

--- intarray.c
+++ intarray.c
@@ -6,7 +6,7 @@
 /*   By: alaamimi <alaamimi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/14 18:17:50 by alaamimi          #+#    #+#             */
-/*   Updated: 2021/09/14 23:06:30 by alaamimi         ###   ########.fr       */
+/*   Updated: 2021/09/15 00:28:19 by alaamimi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -175,19 +175,37 @@ int		intarray_nb_occurences(intarray tab, int n)
 int	main(void)
 {
 	intarray	str = intarray_create(5);
-	intarray	toto = intarray_create(10);
-	intarray	titi = intarray_create(10);
+	intarray	toto = intarray_create(3);
+	intarray	titi = intarray_create(4);
 	intarray_set(str, 0, 42);
 	intarray_set(str, 1, -42);
 	intarray_set(str, 2, 1337);
 	intarray_set(str, 3, -1337);
 	intarray_set(str, 4, 19);
+
+	intarray_set(toto, 0, 42);
+	intarray_set(toto, 1, 1337);
+	intarray_set(toto, 2, 101);
+
+	intarray_set(titi, 0, -42);
+	intarray_set(titi, 1, 69);
+	intarray_set(titi, 2, 19);
+	intarray_set(titi, 3, -19);
+
 	intarray_debug(str);
 	printf("\n%d\n", intarray_get(str, 2));
 	intarray_print_positive_values(str);
 	printf("\n");
 	printf("%d", intarray_length(str));
 	printf("\n");
+	intarray_debug(toto);
+	printf("\n");
+	intarray_debug(titi);
+	printf("\n");
+	intarray_debug(intarray_concat(toto, titi));
+	printf("\n");
+	intarray_destroy(toto);
+	intarray_destroy(titi);
 	intarray_destroy(str);
 	return (EXIT_SUCCESS);
 }
