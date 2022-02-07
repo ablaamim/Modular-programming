d32e7a0c6758d415589a9aaf85dca6b0fd2347c3
ablaamim <aku-no-hana@outlook.fr>
2022-02-07 15:44:01 +0100

    adjust readme

--- intarray/ft_intarray_sort.c
+++ intarray/ft_intarray_sort.c
@@ -6,7 +6,7 @@
 /*   By: ablaamim <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 14:47:14 by ablaamim          #+#    #+#             */
-/*   Updated: 2022/02/07 15:20:36 by ablaamim         ###   ########.fr       */
+/*   Updated: 2022/02/07 15:42:42 by ablaamim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,21 +34,4 @@ void	ft_intarray_sort(t_intarray tab)
 		}
 		i++;
 	}
-}
-
-int	main(int argc, char **argv)
-{
-	(void)	argc;
-	(void)	argv;
-	t_intarray tab = ft_intarray_create(4);
-	ft_intarray_set(tab, 0, 42);
-	ft_intarray_set(tab, 1, -42);
-	ft_intarray_set(tab, 2, 1337);
-	ft_intarray_set(tab, 3, 0);
-	ft_intarray_set(tab, 4, 101);
-	ft_intarray_debug(tab);
-	write(1, "\n", 1);
-	ft_intarray_sort(tab);
-	ft_intarray_debug(tab);
-	return (EXIT_SUCCESS);
 }
